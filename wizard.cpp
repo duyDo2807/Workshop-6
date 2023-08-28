80dbcb5fce2a9fa9241880ffd3f2fcf4027faf31
duyDo2807 <a1881078@adelaide.edu.au>
2023-08-28 15:46:08 +0930

    Workshop 6

--- wizard.cpp
+++ wizard.cpp
@@ -4,10 +4,15 @@ Wizard::Wizard(std::string name, int health, int damage, int mana)
     : Player(name, health, damage), mana(mana) {}
 
 void Wizard::castSpell(Player* opponent) {
-  if (mana > 0) {
+  if (mana > getDamage()) {
     std::cout << getName() << " casts a spell on " << opponent->getName()
               << " for " << getDamage() << " damage.\n";
     opponent->takeDamage(getDamage());
     mana = mana - getDamage();
+  } else if (mana > 0) {
+    std::cout << getName() << " casts a spell on " << opponent->getName()
+              << " for " << mana << " damage.\n";
+    opponent->takeDamage(mana);
+    mana = mana - mana;
   }
 }
